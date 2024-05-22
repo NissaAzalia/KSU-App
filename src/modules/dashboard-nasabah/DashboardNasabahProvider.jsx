05014d779c50ad02e306ae75ab70c66fa0f9b9dd
AmelFatkhatul <amel227091@student.smkn1kandeman.sch.id>
2024-05-22 15:24:46 +0700

    Fix Conflict

--- src/modules/dashboard-nasabah/DashboardNasabahProvider.jsx
+++ src/modules/dashboard-nasabah/DashboardNasabahProvider.jsx
@@ -1,200 +1,157 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react-refresh/only-export-components */
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/prop-types */
 
 import { createContext, useContext, useEffect, useState } from "react"
-import { apiFetchPinjaman, apiFetchServis, apiFetchBeliBarang, apiFetchPinjamUang, apiFetchSimpanan } from "./request"
+import { apiFetchBeliBarang, apiFetchPinjaman, apiFetchServis, apiFetchSimpanan } from "./request"
 import Swal from "sweetalert2"
 
 
 const initDashboardNasabah = {
-  simpanan: null,
-  pinjaman: null,
-  servis: false,
-  beliBarang: false,
-  pinjamUang: false,
-  loadingSimpanan: false,
-  loadingPinjaman: false,
-  loadingServis: false,
-  loadingBeliBarang: false,
-  loadingUang: false,
-
-  fetchSimpanan: () => { },
-  fetchPinjaman: () => { },
-  fetchServis: () => { },
-  fetchBeliBarang: () => { },
-  fetchPinjamUang: () => { },
+    simpanan: null,
+    pinjaman: null,
+    servis:false,
+    beliBarang:false,
+    loadingSimpanan: false,
+    loadingPinjaman: false,
+    loadingServis:false,
+    loadingBeliBarang:false,
+    fetchSimpanan: () => {},
+    fetchPinjaman: () => {},
+    fetchServis:() => {},
+    fetchBeliBarang:() => {},
 
 }
 
 const DashboardNasabahContext = createContext(initDashboardNasabah)
 
 const useDashboardNasabah = () => {
-  return useContext(DashboardNasabahContext)
+    return useContext(DashboardNasabahContext)
 }
 
 
-const DasboardNasabahProvider = ({ children }) => {
-  const [simpanan, setSimpanan] = useState(null)
-  const [pinjaman, setPinjaman] = useState(null)
-  const [servis, setServis] = useState(false)
-  const [pinjamUang, setPinjamUang] = useState(false)
-  const [beliBarang, setBeliBarang] = useState(false)
-  const [loadingSimpanan, setLoadingSimpanan] = useState(false)
-  const [loadingPinjaman, setLoadingPinjaman] = useState(false)
-  const [loadingServis, setLoadingServis] = useState(false)
-  const [loadingBeliBarang, setLoadingBeliBarang] = useState(false)
-  const [loadingUang, setLoadingUang] = useState(false)
+const DasboardNasabahProvider = ({children}) => {
+    const [simpanan, setSimpanan] = useState(null)
+    const [pinjaman, setPinjaman] = useState(null)
+    const [servis, setServis] = useState(false)
+    const [beliBarang, setBeliBarang] = useState(false)
+    const [loadingSimpanan, setLoadingSimpanan] = useState(false)
+    const [loadingPinjaman, setLoadingPinjaman] = useState(false)
+    const [loadingServis, setLoadingServis] = useState(false)
+    const [loadingBeliBarang, setLoadingBeliBarang] = useState(false)
 
-  const fetchSimpanan = async () => {
-    if (loadingSimpanan == true) return
+    const fetchSimpanan = async () => {
+        if (loadingSimpanan == true) return
 
-    setLoadingSimpanan(true)
-    // call api
-    const apiCall = await apiFetchSimpanan()
-    const { data } = apiCall.data
+        setLoadingSimpanan(true)
+        // call api
+        const apiCall = await apiFetchSimpanan()
+        const {data} = apiCall.data
 
-    setSimpanan(data.simpanan)
+        setSimpanan(data.simpanan)
 
-    setLoadingSimpanan(false)
-  }
-
-  const fetchPinjaman = async () => {
-    if (loadingPinjaman == true) return
-
-    setLoadingPinjaman(true)
-    // call api
-    const apiCall = await apiFetchPinjaman()
-    const { data } = apiCall.data
-
-    setPinjaman(data.pinjaman)
-    console.log(data.pinjaman)
-    setLoadingPinjaman(false)
-  }
-
-  const doServis = async (jenisBarang, alamat, jenisKerusakan) => {
-    // cek loading
-    if (loadingServis) return
-
-    // set loading true
-    setLoadingServis(true)
-
-    // tampilkan loading pake swal
-    Swal.fire({
-      title: "Loading",
-      text: "Mengirim data.."
-    })
-    Swal.showLoading()
-
-    // fetch api
-    const apiResult = await apiFetchServis(jenisBarang, alamat, jenisKerusakan)
-    const { status, data, message } = apiResult.data
-
-    // cek sukses / error
-    if (status != 'success') {
-      Swal.hideLoading()
-      Swal.fire({
-        title: `Gagal mengirim service`,
-        text: message,
-        showConfirmButton: true
-      })
+        setLoadingSimpanan(false)
     }
 
-    // set loading false
-    setLoadingServis(false)
+    const fetchPinjaman = async () => {
+        if (loadingPinjaman == true) return
 
-    // hilangkan tampilan loading
-    Swal.hideLoading()
-    Swal.fire({
-      title: 'Sukses',
-      text: 'Berhasil mengirim data servis'
-    })
+        setLoadingPinjaman(true)
+        // call api
+        const apiCall = await apiFetchPinjaman()
+        const {data} = apiCall.data
 
-    // selesai
+        setPinjaman(data.pinjaman)
+        console.log(data.pinjaman)
+        setLoadingPinjaman(false)
+    }
 
-  }
+    const doServis = async (jenisBarang, alamat, jenisKerusakan) => {
+      // cek loading
+      if (loadingServis) return
 
+      // set loading true
+      setLoadingServis(true)
 
-  const doPinjamUang = async (jumlah, tenor) => {
-    // cek loading
-    if (loadingUang) return
+      // tampilkan loading pake swal
+      Swal.fire({
+        title: "Loading",
+        text: "Mengirim data.."
+      })
+      Swal.showLoading()
 
-    // set loading true
-    setLoadingUang(true)
+      // fetch api
+      const apiResult = await apiFetchServis(jenisBarang, alamat, jenisKerusakan)
+      const { status, message} = apiResult.data
 
-    // tampilkan loading pake swal
-    Swal.fire({
-      title: "Loading",
-      text: "Mengirim data.."
-    })
-    Swal.showLoading()
-    // fetch api
-    const apiResult = await apiFetchPinjamUang(jumlah, tenor)
-    const { data, status, message } = apiResult.data
+      // cek sukses / error
+      if (status != 'success') {
+        Swal.hideLoading()
+        Swal.fire({
+          title: `Gagal mengirim service`,
+          text: message,
+          showConfirmButton: true
+        })
+      }
 
-    // cek sukses / error
-    if (status != 'success') {
+      // set loading false
+      setLoadingServis(false)
+
+      // hilangkan tampilan loading
       Swal.hideLoading()
       Swal.fire({
-        title: `Gagal mengirim data`,
-        text: message,
-        showConfirmButton: true
+        title: 'Sukses',
+        text: 'Berhasil mengirim data servis'
       })
+
+      // selesai
+
     }
-    // set loading false
-    setLoadingUang(false)
 
-    // hilangkan tampilan loading
-    Swal.hideLoading()
-    Swal.fire({
-      title: 'Sukses',
-      text: 'Berhasil mengirim data pinjam uang'
-    })
+    const doBeliBarang = async (nama_barang, alamat, jumlah_barang) => {
+      if (loadingBeliBarang) return
+      setLoadingBeliBarang(true)
 
-    // selesai
+      Swal.fire({
+        title: "Loading",
+        text: "Mengirim data.."
+      })
+      Swal.showLoading()
 
-  }
+      const apiResult = await apiFetchBeliBarang(nama_barang, alamat, jumlah_barang)
+      const {status, message} = apiResult.data
 
-  const doBeliBarang = async (nama_barang, alamat, jumlah_barang) => {
-    if (loadingBeliBarang) return
-    setLoadingBeliBarang(true)
-    Swal.fire({
-      title: "Loading",
-      text: "Mengirim data.."
-    })
-    Swal.showLoading()
-    const apiResult = await apiFetchBeliBarang(nama_barang, alamat, jumlah_barang)
-    const { data, status, message } = apiResult.data
+      if (status != 'success') {
+        Swal.hideLoading()
+        Swal.fire({
+          title: 'Gagal mengirim service',
+          text: message,
+          showConfirmButton: true
+        })
+      }
 
-    if (status != 'success') {
+      setLoadingBeliBarang(false)
+
       Swal.hideLoading()
       Swal.fire({
-        title: 'Gagal mengirim data',
-        text: message,
-        showConfirmButton: true
-      })
-    }
-    setLoadingBeliBarang(false)
+        title: 'Sukses',
+        text: 'Berhasil mengirim data Beli Barang'
+})
+}
+  
+    useEffect(() => {
+        fetchPinjaman()
+        fetchSimpanan()
+    }, [])
+    
 
-    Swal.hideLoading()
-    Swal.fire({
-      title: 'Sukses',
-      text: 'Berhasil mengirim data Beli Barang'
-    })
-  }
-
-  useEffect(() => {
-    fetchPinjaman()
-    fetchSimpanan()
-  }, [])
-
-
-  return (
-    <DashboardNasabahContext.Provider value={{ simpanan, pinjaman, servis, beliBarang, pinjamUang, setServis, doServis, setBeliBarang, setPinjamUang, doBeliBarang, doPinjamUang, loadingSimpanan, loadingPinjaman, loadingServis, loadingBeliBarang, loadingUang }}>
-      {children}
-    </DashboardNasabahContext.Provider>
-  )
-
+    return (
+        <DashboardNasabahContext.Provider value={{simpanan, pinjaman, servis, beliBarang, setServis, doServis, setLoadingBeliBarang, doBeliBarang, loadingSimpanan, loadingPinjaman, loadingServis, loadingBeliBarang}}>
+            {children}
+        </DashboardNasabahContext.Provider>
+    )
+    
 }
 
-export { useDashboardNasabah, DasboardNasabahProvider }
+export {useDashboardNasabah, DasboardNasabahProvider}
